6f1421c62c7c88a8f88dd1d54a67d1d5b085c93d
Madhukar kumar <madhukarkumar2468@gmail.com>
2024-02-07 12:09:19 +0530

    code

--- react-quiz-app/src/App.js
+++ react-quiz-app/src/App.js
@@ -1,15 +1,30 @@
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 import './App.css';
 import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Home from "./Pages/Home/Home";
+import Quiz from "./Pages/Quiz/Quiz";
+import Result from "./Pages/Result/Result";
 
 function App() {
   return (
     <BrowserRouter>
     <div className="app" style={{backgroundImage: "url(./ques1.png)" }} >
     <Header/>
+
+    <Routes>
+      <Route path="/" exact element={<Home/>}>
+      </Route>
+      <Route path="/quiz" exact element={<Quiz/>}>
+      </Route>
+      <Route path="/result" exact element={<Result/>}>
+      </Route>
+    </Routes>
+
+
     </div>
-    
+    <Footer/>
     </BrowserRouter>
   );
 }
---
+++ react-quiz-app/src/Components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div style={{ textAlign:"center", marginBottom:10}}>
+      Made with ♥ by{" "}
+      <a href='https://github.com/Madhukarkrgithub'
+      style={{cursor:"pointer"}}
+      >
+        Madhukar kumar
+      </a>
+    </div>
+  )
+}
+
+export default Footer
